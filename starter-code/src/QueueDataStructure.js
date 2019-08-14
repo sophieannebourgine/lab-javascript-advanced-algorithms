9a22d9063bc58c59443e32ed08b4f432e4e95d83
sophieannebourgine <sophieannebourgine@gmail.com>
2019-08-14 15:29:20 +0200

    fini

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,42 @@
 function QueueDataStructure () {
+    this.queueControl=[];
+    this.MAX_SIZE=9;
+}
+
+QueueDataStructure.prototype.isEmpty=function () {
+    if (this.queueControl.length ===0) {
+        return true;  
+    }
+    else {
+        return false;
+    }
+}
+
+QueueDataStructure.prototype.canEnqueue=function() {
+    if(this.MAX_SIZE>this.queueControl.length){
+        return true
+    }
+    else {
+        return false 
+    }
+}
+QueueDataStructure.prototype.enqueue = function (n) {
+    if (this.canEnqueue()) {
+        this.queueControl.push(n);
+        this.queueControl.reverse();
+
+        return this.queueControl;
+    }
+    else {
+        return "Queue Overflow"
+    }
+}
+
+QueueDataStructure.prototype.dequeue = function () {
+    if (this.queueControl.length>0) {
+        return this.queueControl.pop();
+    }
+    else {
+        return "Queue Underflow";
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,44 @@
-function StackDataStructure () {
+function StackDataStructure() {
+    this.stackControl = [];
+    this.MAX_SIZE = 9;
+}
+
+StackDataStructure.prototype.isEmpty = function () {
+    if (this.stackControl.length === 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+
+StackDataStructure.prototype.canPush = function () {
+    if (this.MAX_SIZE > this.stackControl.length) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+StackDataStructure.prototype.push = function (n) {
+    if (this.canPush()) {
+        this.stackControl.push(n);
+        return this.stackControl;
+    }
+    else {
+        return "Stack Overflow"
+    }
+}
+
+
+StackDataStructure.prototype.pop = function (n) {
+    if (this.stackControl.length>0) {
+        
+        return this.stackControl.pop();
+    }
+    else {
+        return "Stack Underflow";
+    }
 }
